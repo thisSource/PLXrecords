d18e2bc077e8c0099c5c3510577ef55883dacef1
Alexander Höglund <alexander.hoglund@gmail.com>
2022-10-03 23:06:57 +0200

    update

--- pages/index2.jsx
+++ pages/index2.jsx
@@ -2,12 +2,11 @@ import Background from "../components/index/background";
 import MobileBackground from "../components/index/MobileBackground";
 import Clicked from "../components/index/Clicked";
 import React, { useEffect, useState } from "react";
-import OceansBackground from "../components/index/oceansBackground";
+import OceansBackground from "../components/index/OceansBackground";
 import OceansBackgroundMobile from "../components/index/OceansBackgroundMobile";
 
 function Index() {
   const [isMobile, setIsMobile] = useState(true);
-  
 
   useEffect(() => {
     window.innerWidth < 600 ? setIsMobile(true) : setIsMobile(false);
